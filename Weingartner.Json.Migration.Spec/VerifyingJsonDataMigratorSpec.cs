084b04be7c541a7f6a41131a859e48ab141d6b1e
Johannes Egger <eggerjohansi@gmail.com>
2015-05-08 11:03:43 +0200

    Add `VerifyingJsonDataMigrator`

---
+++ Weingartner.Json.Migration.Spec/VerifyingJsonDataMigratorSpec.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Weingartner.Json.Migration.Spec
+{
+    public class VerifyingJsonDataMigratorSpec
+    {
+        [Theory]
+        [InlineData("{'Street': 'Teststreet', 'Number': 5, 'Prop': 'Whatever'}", typeof(Address))]
+        [InlineData("[{'Street': 'Teststreet', 'Number': 5, 'Prop': 'Whatever'}]", typeof(IEnumerable<Address>))]
+        public void ShouldFailVerificationWhenPropertyWasntRemoved(string jsonData, Type type)
+        {
+            var obj = JToken.Parse(jsonData);
+            var sut = GetVerifyingMigrator();
+
+            new Action(() => sut.TryMigrate(ref obj, type)).ShouldThrow<MigrationException>();
+        }
+
+        [Theory]
+        [InlineData("{'Street': 'Teststreet'}", typeof(Address))]
+        [InlineData("[{'Street': 'Teststreet'}]", typeof(IEnumerable<Address>))]
+        public void ShouldFailVerificationWhenDataPropertyIsMissing(string jsonData, Type type)
+        {
+            var obj = JToken.Parse(jsonData);
+            var sut = GetVerifyingMigrator();
+
+            new Action(() => sut.TryMigrate(ref obj, type)).ShouldThrow<MigrationException>();
+        }
+
+        [Theory]
+        [InlineData("{'Street': 'Teststreet', 'Number': 5}", typeof(Address))]
+        [InlineData("[{'Street': 'Teststreet', 'Number': 5}]", typeof(IEnumerable<Address>))]
+        public void ShouldSucceedVerificationWhenDataIsOk(string jsonData, Type type)
+        {
+            var obj = JToken.Parse(jsonData);
+            var sut = GetVerifyingMigrator();
+
+            new Action(() => sut.TryMigrate(ref obj, type)).ShouldNotThrow<MigrationException>();
+        }
+
+        private static IMigrateData<JToken> GetVerifyingMigrator()
+        {
+            return new VerifyingJsonDataMigrator(new NullMigrator());
+        }
+
+        // ReSharper disable UnusedMember.Local
+        private class Address
+        {
+            public string Street { get; set; }
+            public int Number { get; set; }
+        }
+        // ReSharper restore UnusedMember.Local
+
+        private class NullMigrator : IMigrateData<JToken>
+        {
+            public void TryMigrate(ref JToken data, Type dataType)
+            {
+            }
+        }
+    }
+}
---
+++ Weingartner.Json.Migration/VerifyingJsonDataMigrator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace Weingartner.Json.Migration
+{
+    public class VerifyingJsonDataMigrator : IMigrateData<JToken>
+    {
+        private readonly IMigrateData<JToken> _Inner;
+
+        public VerifyingJsonDataMigrator(IMigrateData<JToken> inner)
+        {
+            _Inner = inner;
+        }
+
+        public void TryMigrate(ref JToken data, Type dataType)
+        {
+            _Inner.TryMigrate(ref data, dataType);
+            VerifyMigration(data, dataType);
+        }
+
+        private static void VerifyMigration(JToken data, Type dataType)
+        {
+            if (data is JArray)
+            {
+                if (!dataType.IsGenericType)
+                {
+                    // We can't verify anything if we don't know the type
+                    return;
+                }
+                // Let's assume that the first generic argument refers to the element type
+                var childType = dataType.GetGenericArguments().First();
+                foreach (var child in data)
+                {
+                    VerifyMigration(child, childType);
+                }
+                return;
+            }
+
+            var dataProperties = data.Children<JProperty>().Select(p => p.Name);
+            var typeProperties = dataType.GetProperties(BindingFlags.Instance | BindingFlags.Public).Select(p => p.Name);
+            var superfluousDataProperties = dataProperties.Except(typeProperties).ToList();
+            if (superfluousDataProperties.Count > 0)
+            {
+                throw new MigrationException(string.Format("The following properties should be removed from the serialized data because they don't exist in type {0}: {1}", dataType.FullName, string.Join(", ", superfluousDataProperties)));
+            }
+
+            var missingDataProperties = typeProperties.Except(dataProperties).ToList();
+            if (missingDataProperties.Count > 0)
+            {
+                throw new MigrationException(string.Format("The following properties should be added to the serialized data because they exist in type {0}: {1}", dataType.FullName, string.Join(", ", missingDataProperties)));
+            }
+        }
+    }
+}
