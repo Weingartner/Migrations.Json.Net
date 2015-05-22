0d8bebd11ffa1eca57a569935773b8b2e0571270
Johannes Egger <eggerjohansi@gmail.com>
2015-05-22 06:50:39 +0200

    Revert "Add nuspec for dummy weaver"
    This reverts commit 051e7778cc000052629b93bc58a54d8a170c0442.

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,2 +1,1 @@
 nuget pack Weingartner.Json.Migration.Fody.nuspec -OutputDirectory ..\..\NuGet -Version 0.0.1-alpha
-nuget pack DummyWeaver.Fody.nuspec -OutputDirectory ..\..\NuGet -Version 0.0.1-alpha
