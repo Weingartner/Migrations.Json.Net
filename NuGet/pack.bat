051e7778cc000052629b93bc58a54d8a170c0442
Johannes Egger <eggerjohansi@gmail.com>
2015-05-22 06:29:49 +0200

    Add nuspec for dummy weaver

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -1,1 +1,2 @@
 nuget pack Weingartner.Json.Migration.Fody.nuspec -OutputDirectory ..\..\NuGet -Version 0.0.1-alpha
+nuget pack DummyWeaver.Fody.nuspec -OutputDirectory ..\..\NuGet -Version 0.0.1-alpha
