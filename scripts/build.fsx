47a73d74ae5f674bb8dc86e330413a929b3bbf27
Johannes Egger <eggerjohansi@gmail.com>
2016-10-19 13:51:15 +0200

    Simplify xunit runner path

--- scripts/build.fsx
+++ scripts/build.fsx
@@ -40,7 +40,7 @@ Target "BuildSolution" (fun () ->
 Target "RunTests" (fun () ->
     let setParams (p: XUnit2Params) =
         { p with
-            ToolPath = currentDirectory @@ @"lib\xunit.runner.console\tools\xunit.console.exe"
+            ToolPath = SOURCE_DIRECTORY @@ @"lib\xunit.runner.console\tools\xunit.console.exe"
             Parallel = ParallelMode.All
             ErrorLevel = Error
         }
