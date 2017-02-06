e4afb497f1a0f6043214fc6dd85fc18a9051be7a
bradphelan <b.phelan@weingartner.com>
2017-02-06 12:07:08 +0100

    Fix path for build tools

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 REM If GitVersion is not available then run 
 REM cinst GitVersion.Portable
 REM to install it
-GitVersion.exe /exec BuildTools\lib\FAKE\tools\Fake.exe /execArgs "scripts\build.fsx %1" /l console
+GitVersion.exe /exec scripts\lib\FAKE\tools\Fake.exe /execArgs "scripts\build.fsx %1" /l console
