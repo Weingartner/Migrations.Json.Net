b2bea4b9d1b5dfe543f2edd625c1143c98f7b1c2
bradphelan <b.phelan@weingartner.com>
2017-05-10 09:25:34 +0200

    Allow more arguments to fake

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 REM If GitVersion is not available then run 
 REM cinst GitVersion.Portable
 REM to install it
-GitVersion.exe /output buildserver  /exec scripts\lib\FAKE\tools\Fake.exe /execArgs "scripts\build.fsx %1" /l console
+GitVersion.exe /output buildserver  /exec scripts\lib\FAKE\tools\Fake.exe /execArgs "scripts\build.fsx %1 %2 %3 %4 %5 %6 %7 %8 %9 " /l console
