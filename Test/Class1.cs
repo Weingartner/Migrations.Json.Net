0522e76e47ee8f8cb1d6f1560e42fed2a6daffb8
bradphelan <b.phelan@weingartner.com>
2017-10-12 12:13:12 +0200

    Add a test project for interactive testing

---
+++ Test/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Weingartner.Json.Migration;
+using Weingartner.Json.Migration.Common;
+
+namespace Test
+{
+    [Migratable("758832613")]
+    [DataContract]
+    public class Class1
+    {
+        [DataMember]
+        private int i { get; set; }
+    }
+}
