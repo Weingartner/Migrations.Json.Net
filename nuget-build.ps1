a34f7c624a7d3b7b406bf829bfe546a13fb8dc03
Florian <f.auinger@weingartner.com>
2022-03-17 09:15:25 +0100

    remove unused file

--- nuget-build.ps1
+++
@@ -1,9 +0,0 @@
-$version=gitversion /output json /showvariable FullSemVer
-$csprojdir="Weingartner.Json.Migration_"
-$csproj="$csprojdir\Weingartner.Json.Migration.csproj"
-$pkg="$csprojdir\artifacts\Weingartner.Json.Migration.$version.nupkg"
-dotnet pack -o ./artifacts  --configuration Release /p:Version=$version $csproj
-Write-Host $pkg
-invoke-item $pkg
-cp $pkg ../Nuget
-dotnet nuget push $pkg
